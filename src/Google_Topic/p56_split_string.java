91da07c8ad84fd9a5cc75b8f7db9c206630807e4
xiaoran.hu <huxiaora@usc.edu>
2016-02-09 16:27:03 -0800

    2/9/2016

--- src/Google_Topic/p56_split_string.java
+++ src/Google_Topic/p56_split_string.java
@@ -1,5 +1,6 @@
 package Google_Topic;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -9,12 +10,31 @@ import java.util.List;
 public class p56_split_string {
 
     public static List<String> splitString(String str){
-        
-        return null;
+        List<String> res = new ArrayList<>();
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < str.length(); i++){
+        	char ch = str.charAt(i);
+        	if (ch == '\"'){
+        		int lookAheadIdx = i+1;
+        		while (lookAheadIdx < str.length() && str.charAt(lookAheadIdx) != '\"') lookAheadIdx++;
+        		res.add(str.substring(i+1,lookAheadIdx));
+        		i = lookAheadIdx;
+        	}else if (ch == ' '){
+        		while ( i+1 < str.length() && str.charAt(i+1) == ' ') i++;
+        		if (builder.length() != 0){
+        			res.add(builder.toString());
+        			builder.setLength(0);
+        		}
+        	}else{
+        		builder.append(ch);
+        	}
+        }
+        if (builder.length() != 0) res.add(builder.toString());
+        return res;
     }
 
     public static void main(String[] args) {
         String s = "Location \"Welcome to india\" Bangalore Channai \"IT city\" Mysore";
-//        splitString(s);
+        System.out.println(splitString(s));
     }
 }
---
+++ src/Google_Topic/p62_hashTableWithRandom.java
@@ -0,0 +1,65 @@
+package Google_Topic;
+
+import java.util.*;
+
+/**
+ * Created by stameying on 2/9/16.
+ */
+public class p62_hashTableWithRandom<K,V>{
+
+    Map<K,V> KeyValueMap;
+    Map<K,Integer> KeyIndexMap;
+    List<K> keyArr;
+    Random ran;
+    public p62_hashTableWithRandom(){
+        KeyValueMap = new HashMap<>();
+        KeyIndexMap = new HashMap<>();
+        keyArr = new ArrayList<>();
+        ran = new Random();
+    }
+    public V get(K key){
+        if (KeyValueMap.containsKey(key)) return KeyValueMap.get(key);
+        else return null;
+    }
+
+    public V put(K key, V val){
+        if (KeyValueMap.containsKey(key)){
+            return KeyValueMap.put(key,val);
+        }else{
+            KeyValueMap.put(key,val);
+            keyArr.add(key);
+            KeyIndexMap.put(key,keyArr.size()-1);
+            return null;
+        }
+    }
+
+    public V remove(K key){
+        if (KeyValueMap.containsKey(key)){
+            Integer idx = KeyIndexMap.get(key);
+            KeyIndexMap.remove(key);
+            Collections.swap(keyArr, idx, keyArr.size()-1);
+            keyArr.remove(keyArr.size()-1);
+            return KeyValueMap.remove(key);
+        }else{
+            return null;
+        }
+    }
+
+    public V getRandom(){
+        int idx = ran.nextInt(keyArr.size());
+        K key = keyArr.get(idx);
+        return KeyValueMap.get(key);
+    }
+
+
+    public static void main(String[] args) {
+        p62_hashTableWithRandom<Integer,Integer> randomMap = new p62_hashTableWithRandom<>();
+        randomMap.put(2,3);
+        randomMap.put(2,4);
+        randomMap.put(3,2);
+        randomMap.put(1,5);
+        randomMap.put(2,7);
+        System.out.println(randomMap.getRandom());
+    }
+
+}
